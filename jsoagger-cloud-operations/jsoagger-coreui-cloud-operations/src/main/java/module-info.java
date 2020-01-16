aa23f3df7ef63e0b5369ec294a50bacc8b49696c
yvonjisoa <rmvonji@gmail.com>
2020-01-16 21:44:10 +0100

    refactor

--- jsoagger-cloud-operations/jsoagger-coreui-cloud-operations/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module io.github.jsoagger.coreui.cloud.operations {
-	exports io.github.jsoagger.core.ui.cloud.operations.dash;
-
-	requires com.google.gson;
-	requires io.github.jsoagger.core.bridge;
-	requires io.github.jsoagger.coreui.cloud.services;
-}
--- jsoagger-offline-operations/jsoagger-corebusiness-offline-services/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module io.github.jsoagger.corebusiness.offline.services {
-	exports io.github.jsoagger.core.business.offline.services.notifications.impl;
-	exports io.github.jsoagger.core.business.offline.services.notifications.api;
-	exports io.github.jsoagger.core.business.offline.services.notifications;
-
-	requires com.google.gson;
-	requires io.github.jsoagger.core.bridge;
-}
